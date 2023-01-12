b0d2b1a83f4db4dbab742e92ffc95ef5a309b2fe
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2023-01-11 22:38:18 -0600

    Added header, theme toggle and some assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,75 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
-        serif: ['LoraVariable', 'Lora', ...defaultTheme.fontFamily.serif],
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        serif: ["LoraVariable", "Lora", ...defaultTheme.fontFamily.serif],
         heading: [
-          'MontserratVariable',
-          'Montserrat',
+          "MontserratVariable",
+          "Montserrat",
           ...defaultTheme.fontFamily.sans,
         ],
+      },
+      colors: {
+        // rosePine
+        rp: {
+          base: "#191724",
+          surface: "#1f1d2e",
+          overlay: "#26233a",
+          muted: "#6e6a86",
+          subtle: "#908caa",
+          text: "#e0def4",
+          love: "#eb6f92",
+          gold: "#f6c177",
+          rose: "#ebbcba",
+          pine: "#31748f",
+          foam: "#9ccfd8",
+          iris: "#c4a7e7",
+          highlightLow: "#21202e",
+          highlightMed: "#403d52",
+          highlightHigh: "#524f67",
+        },
+        // rosePineMoon
+        rpm: {
+          base: "#232136",
+          surface: "#2a273f",
+          overlay: "#393552",
+          muted: "#6e6a86",
+          subtle: "#908caa",
+          text: "#e0def4",
+          love: "#eb6f92",
+          gold: "#f6c177",
+          rose: "#ea9a97",
+          pine: "#3e8fb0",
+          foam: "#9ccfd8",
+          iris: "#c4a7e7",
+          highlightLow: "#2a283e",
+          highlightMed: "#44415a",
+          highlightHigh: "#56526e",
+        },
+        // rosePineDawn
+        rpd: {
+          base: "#faf4ed",
+          surface: "#fffaf3",
+          overlay: "#f2e9e1",
+          muted: "#9893a5",
+          subtle: "#797593",
+          text: "#575279",
+          love: "#b4637a",
+          gold: "#ea9d34",
+          rose: "#d7827e",
+          pine: "#286983",
+          foam: "#56949f",
+          iris: "#907aa9",
+          highlightLow: "#f4ede8",
+          highlightMed: "#dfdad9",
+          highlightHigh: "#cecacd",
+        },
       },
     },
   },
