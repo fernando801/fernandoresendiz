9d6b8982d5b19ebc16b75acfd9b59ee1de454f6d
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2023-01-15 20:07:31 -0600

    Added image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -9,6 +9,15 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
