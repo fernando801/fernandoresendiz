6812564b82475fbb00aa2292da30eae15f56b5eb
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2023-01-25 20:08:06 -0600

    Upgraded to astro 2.0

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
